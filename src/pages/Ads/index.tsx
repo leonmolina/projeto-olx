b95cfe630dc59304bb2cb5b7ed7492adb6389c25
Leon Molina <leon.maguiar@gmail.com>
2022-03-24 15:34:52 -0300

    added query handle on ads page

--- src/pages/Ads/index.tsx
+++ src/pages/Ads/index.tsx
@@ -1,12 +1,32 @@
 import { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 import useApi, { AdsInterface, Category, State } from '../../helpers/OlxApi';
 import './style.css';
 
 
 const Ads = () => {
-// API CALL AND USE STATES
+// API CALL
     const api = useApi();
+// HANDLES QUERY SEARCH AND FILLS THE STATE VARIABLE
+    const useQueryString = () => {
+        return new URLSearchParams( useLocation().search );
+    }
+    const query = useQueryString();
+    const auxVerifyQuery = (querySearch: string) => {
+        let resultQuery: any = ''
+        if (querySearch == null) {
+            resultQuery = ''
+        } else {
+            resultQuery = query.get(querySearch)
+        }
+        return resultQuery
+        // return query.get(querySearch) != null ? query.get(querySearch) : ''
+    }
+    const [q, setQ] = useState(auxVerifyQuery('q'));
+    const [cat, setCat] = useState(auxVerifyQuery('cat'));
+    const [state, setState] = useState(auxVerifyQuery('state'));
+// USE STATES
     const [stateList, setStateList] = useState<State[]>([]);
     const [categories, setCategories] = useState<Category[]>([]);
     const [adList, setAdList] = useState<AdsInterface[]>([]);
@@ -43,10 +63,14 @@ const Ads = () => {
             <div className="ad-page--area">
                 <div className="ad-page--leftSide">
                     <form method="GET">
-                        <input type="text" name="q" />
-
+                        <input
+                        type="text"
+                        name="q"
+                        placeholder='O que você procura?'
+                        value={q}
+                        />
                         <div className="filterName">Estado:</div>
-                            <select name="state">
+                            <select name="state" value={state}>
                                 <option value=""></option>
                                 {stateList.map((i, k)=>
                                     <option key={k} value={i.name}>{i.name}</option>
@@ -55,7 +79,7 @@ const Ads = () => {
                         <div className="filterName">Categoria:</div>
                         <ul>
                             {categories.map((i, k)=>
-                                <li key={k} className="categoryItem">
+                                <li key={k} className={cat== i.slug?'categoryItem active':'categoryItem'}>
                                     <img src={i.img} alt="Imagem da categoria" />
                                     <span>{i.name}</span>
                                 </li>
