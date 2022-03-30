5182c3a473fff817a0e2f4bf7b47ac88e8e560d6
Leon Molina <leon.maguiar@gmail.com>
2022-03-30 16:53:50 -0300

    updated comments in home page component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,39 +1,39 @@
-// CSS AND BOOTSTRAP
-// REACT
+// REACT HOOKS
 import { useEffect, useState } from 'react';
+// COMPONENTS
 import AdItem from '../../components/partials/AdItem';
-// REQUISITION AND PARTIALS
+// REQUISITION AND TYPES
 import useApi, { AdsInterface, Category, State } from '../../helpers/OlxApi';
+// STYLES
 import * as Styled from './styles';
 
 const Home = () => {
-// API CALL AND USE STATES
+    // API CALL AND USE STATES
     const api = useApi();
     const [stateList, setStateList] = useState<State[]>([]);
     const [categories, setCategories] = useState<Category[]>([]);
     const [adList, setAdList] = useState<AdsInterface[]>([]);
-// STATES - USE EFFECT
-    useEffect(()=>{
+    // GET STATES AND CATEGORIES LIST FROM API
+    useEffect(() => {
         const getStates = async () => {
             const slist = await api.getStates();
             setStateList(slist);
         }
         getStates();
     }, []);
-// CATEGORIES - USE EFFECT
-    useEffect(()=>{
+    useEffect(() => {
         const getCategories = async () => {
             const cats = await api.getCategories();
             setCategories(cats);
         }
         getCategories();
     }, []);
-// ADS - USE EFFECT
-    useEffect(()=>{
+    // GET ADS LIST FROM API
+    useEffect(() => {
         const getRecentAds = async () => {
             const json = await api.getAds({
-                sort:'desc',
-                limit:8
+                sort: 'desc',
+                limit: 8
             });
             setAdList(json.ads);
         }
@@ -49,24 +49,21 @@ const Home = () => {
                     <Styled.SearchBox>
                         {/* SEARCH FORM */}
                         <Styled.SearchForm method="GET" action="/ads">
-                            {/* SEARCH TEXT */}
                             <Styled.SearchInputGroup className='col-lg-9 col-md-6 col-sm-6 px-2'>
                                 <Styled.SearchInput
-                                        type="text"
-                                        className="search-input--text"
-                                        name="q"
-                                        placeholder='O que você procura?'>
+                                    type="text"
+                                    className="search-input--text"
+                                    name="q"
+                                    placeholder='O que você procura?'>
                                 </Styled.SearchInput>
                             </Styled.SearchInputGroup>
-                            {/* SEARCH SELECT */}
                             <Styled.SearchInputGroup className='col-lg-1 col-md-4 col-sm-4 px-2'>
                                 <Styled.SearchSelect>
-                                    {stateList.map((i, k)=>
+                                    {stateList.map((i, k) =>
                                         <option key={k} value={i.name}>{i.name}</option>
                                     )}
                                 </Styled.SearchSelect>
                             </Styled.SearchInputGroup>
-                            {/* SEARCH BUTTON */}
                             <Styled.SearchInputGroup className='col-lg-2 col-md-2 col-sm-2 px-2'>
                                 <Styled.SearchButton variant="primary" type="submit">Pesquisar</Styled.SearchButton>
                             </Styled.SearchInputGroup>
@@ -74,7 +71,7 @@ const Home = () => {
                     </Styled.SearchBox>
                     {/* CATEGORY LIST */}
                     <Styled.CategoryList>
-                        {categories.map((i, k)=>
+                        {categories.map((i, k) =>
                             <Styled.CategoryItem key={k} to={`/ads?cat=${i.slug}`}>
                                 <Styled.CategoryImage src={i.img} alt="Ícone da Categoria" />
                                 <Styled.CategoryName>{i.name}</Styled.CategoryName>
@@ -88,8 +85,8 @@ const Home = () => {
                 <Styled.HomePage>
                     <Styled.HomeTitle>Anúncios Recentes</Styled.HomeTitle>
                     <Styled.HomeList>
-                        {adList.map((i, k)=>
-                            <AdItem key={k} data={i}/>
+                        {adList.map((i, k) =>
+                            <AdItem key={k} data={i} />
                         )}
                     </Styled.HomeList>
                     <Styled.SeeAllLink to="/ads">Ver Todos</Styled.SeeAllLink>
@@ -100,5 +97,4 @@ const Home = () => {
         </>
     );
 }
-
 export default Home;
